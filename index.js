c74655c4dd9cb6ec955f14ee3da7d91e2c99181d
Paul <p@x128.ru>
2023-08-05 10:39:29 +0300

    index upd

--- index.js
+++ index.js
@@ -3,32 +3,8 @@ const testModule = require('./src/Test.js');
 
 const expect = (value) => new Expectation(value);
 
-module.exports = {
-    expect,
-    ...testModule,
-};
-
-console.log('testing..');
-
-const a = {
-    b: 2,
-    b12: {
-        some: 'some',
-    },
-};
-
-const a_ = {
-    b: '2',
-};
-
-testModule.test('some', [
-    expect(a).toEqual(a_),
-    expect(a).toEqualStrict(a_),
-    expect(a).toBeDefined(),
-    expect(a).toBeNull(),
-    expect(a).toBeUndefined(),
-    expect(123).toBeNaN(),
-    expect(a).toHaveProperty('b12.some'),
-]);
-
-testModule.afterAll(testModule.printResult);
+exports.expect = expect;
+exports.test = testModule.test;
+exports.afterAll = testModule.afterAll;
+exports.printResult = testModule.printResult;
+exports.resetTests = testModule.resetTests;
